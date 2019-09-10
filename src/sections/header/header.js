3e439c43cf3b74102f076b4766ee6ee05964a727
siddhi vinayaka <svinayaka290489@gmail.com>
2019-09-11 01:07:27 +0530

    updated files

---
+++ src/sections/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>I am header!</div>
+        )
+    }
+}
+
+
+export default Header;
