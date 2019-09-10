1501658007e89a68c9b40f162602b8160450d919
siddhi vinayaka <svinayaka290489@gmail.com>
2019-09-11 01:33:42 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import Header from './sections/header/header';
+import Footer from './sections/footer/footer';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Header></Header>
+      <Fragment>
+        <Header></Header>
+        <Footer></Footer>
+      </Fragment>
     );
   }
 }
---
+++ src/sections/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">I am footer</div>
+        )
+    }
+}
+
+export default Footer;
