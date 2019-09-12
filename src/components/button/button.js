c37ebbc4f140a1144b83115150b763c8b2f28892
siddhi vinayaka <svinayaka290489@gmail.com>
2019-09-13 02:28:56 +0530

    update files

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button as btn , ButtonToolbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onClicked = this.onClicked.bind(this);
+    }
+    onClicked() {
+        debugger;
+    }
     render() {
         return (
-            <button className="{ this.props.className }">☰</button>
+            <button className="{ this.props.className }" onClick={this.onClicked}>☰</button>
         );
     }
 }
