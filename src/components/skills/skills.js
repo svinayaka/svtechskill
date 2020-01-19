04b69a861be3cce4d7a7adbdd381da2f182afe4e
siddhi vinayaka <svinayaka290489@gmail.com>
2020-01-19 23:04:03 +0530

    update discription notes

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -5,9 +5,16 @@ class SkillComponent extends React.Component {
     constructor(props) {
         super(props);
     }
+
     render() {
         const searchTerm = this.props.txt;
-    return <p>{searchTerm}</p>;
+        const txt = this.props.discription;
+        return (
+            <div>
+                <p>{searchTerm}</p>
+                <pre>{txt}</pre>
+            </div>
+        );
     }
 }
 
